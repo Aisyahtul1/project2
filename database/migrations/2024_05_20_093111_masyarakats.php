96ada8e566a4172fefd78b4530c83c71bd4868eb
Aisyahtul1 <164297508+Aisyahtul1@users.noreply.github.com>
2024-05-20 01:59:21 -0800

    membuat migration

--- database/migrations/2024_05_20_093111_masyarakats.php
+++ database/migrations/2024_05_20_093111_masyarakats.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('masyarakats', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('hak_akses', ['admin', 'user'])->default('user');
-            $table->rememberToken();
+            $table->string('nik')->unique();
+            $table->string('nama');
+            $table->string('jenis_kelamin');
+            $table->string('pekerjaan');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        //
     }
 };
